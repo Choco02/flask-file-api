a2790002a01b4bf815ee4c52a2ca52dbb5d06277
Choco02 <sugarzx01@gmail.com>
2021-08-20 03:26:35 -0300

    Util class created

--- app.py
+++ app.py
@@ -5,55 +5,68 @@ from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename, send_from_directory
 import logging
 
-logging.basicConfig(filename='access.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+class Utils:
+    def __init__(self):
 
-status = {
-    'CREATED': 201,
-    'BAD_REQUEST': 400,
-    'NOT_FOUND': 404,
-    'DUPLICATED': 409
-}
+        self.status_response = {
+            'CREATED': 201,
+            'BAD_REQUEST': 400,
+            'NOT_FOUND': 404,
+            'DUPLICATED': 409
+        }
 
+        self.UPLOAD_FOLDER = './files'
+        self.ALLOWED_EXTENSIONS = ( 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif' )
 
-UPLOAD_FOLDER = './files'
-ALLOWED_EXTENSIONS = ( 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif' )
+        logging.basicConfig(filename='access.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.add_url_rule(
-    "/<file>", endpoint="serve_files", build_only=True
-)
+    @staticmethod
+    def remove_image_exif(self, img: FileStorage):
+        image = Image.open(img)
+        return image
 
-def remove_image_exif(img: FileStorage):
-    image = Image.open(img)
-    return image
+    @staticmethod
+    def allowed_file(self, filename: str):
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1].lower() in self.ALLOWED_EXTENSIONS
 
-def allowed_file(filename: str):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    @staticmethod
+    def is_image(self, filename: str):
+        filename.split('.')[1] in self.ALLOWED_EXTENSIONS[2:]
+
+    @staticmethod
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['UPLOAD_FOLDER'] = self.UPLOAD_FOLDER
+        app.add_url_rule(
+            "/<file>", endpoint="serve_files", build_only=True
+        )
+        return app
+
+app = Utils.create_app()
 
 @app.route('/<file>', methods=['GET'])
 def serve_files(file):
     files_folder = app.config['UPLOAD_FOLDER']
     if file not in os.listdir(files_folder):
-        return jsonify({ 'message': 'File Not Found'}), status['NOT_FOUND']
+        return jsonify({ 'message': 'File Not Found' }), Utils.status['NOT_FOUND']
     return send_from_directory(files_folder, file, environ=request.environ)
 
 
 @app.route('/', methods=['POST'])
 def receive_files():
     if 'file' not in request.files:
-        return jsonify({ 'message': 'No files were sent'}), status['BAD_REQUEST']
+        return jsonify({ 'message': 'No files were sent' }), Utils.status['BAD_REQUEST']
     file = request.files['file']
     if not file.filename:
-        return jsonify({ 'message': 'File without name' }), status['BAD_REQUEST']
+        return jsonify({ 'message': 'File without name' }), Utils.status['BAD_REQUEST']
 
-    if file and allowed_file(file.filename):
+    if file and Utils.allowed_file(file.filename):
         filename = secure_filename(file.filename)
         if filename in os.listdir(app.config['UPLOAD_FOLDER']):
-            return jsonify({ 'message': 'File already exists'}), status['DUPLICATED']
-        if filename.split('.')[1] in ALLOWED_EXTENSIONS[2:]:
-            file = remove_image_exif(file)
+            return jsonify({ 'message': 'File already exists' }), Utils.status['DUPLICATED']
+        if Utils.is_image(filename):
+            file = Utils.remove_image_exif(file)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         data = url_for('serve_files', file=filename, _external=True)
-        return jsonify({ 'message': data }), status['BAD_REQUEST']
+        return jsonify({ 'message': data }), Utils.status['BAD_REQUEST']
